ae93fdb64ebc803cc930c492494c668cfdaf3b76
Teng Teng <tengteng8132002@gmail.com>
2022-11-04 18:08:37 +0800

    Version 3
    Bootstrap and CSS are added.

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.navbar-nav').on('click', 'a', function(){
+        $('.navbar-nav a.active').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
+
